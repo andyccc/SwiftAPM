946d376c1e8dd92f4c428c7a4c651904d4338e62
world <aoyan2010@live.cn>
2019-09-18 14:29:07 +0800

    add codes

---
+++ Example/SwiftAPM/TestOC.h
@@ -0,0 +1,20 @@
+//
+//  TestOC.h
+//  SwiftAPM_Example
+//
+//  Created by yans on 2019/9/18.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestOC : NSObject
+
+- (void)testDemo;
+
+- (instancetype)initWithData:(NSString *)data;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/SwiftAPM/TestOC.m
@@ -0,0 +1,25 @@
+//
+//  TestOC.m
+//  SwiftAPM_Example
+//
+//  Created by yans on 2019/9/18.
+//
+
+#import "TestOC.h"
+
+@implementation TestOC
+
+- (instancetype)initWithData:(NSString *)data
+{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+- (void)testDemo {
+    NSLog(@"------------>");
+}
+
+@end
